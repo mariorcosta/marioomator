56c5f2d01fcdbcb6dc78908fdcd1ff960a7efe4b
mariorcosta <mario_costa_97@hotmail.com>
2022-10-17 22:55:51 +0100

    asd

--- src/stories/Header.stories.tsx
+++ src/stories/Header.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import { Header } from './Header';
+import { Header } from '../components/Headers/Header';
 
 export default {
   title: 'Example/Header',
@@ -17,7 +17,7 @@ const Template: ComponentStory<typeof Header> = (args) => <Header {...args} />;
 export const LoggedIn = Template.bind({});
 LoggedIn.args = {
   user: {
-    name: 'Mário Costa',
+    name: 'Mário Costa  ',
   },
 };
 
